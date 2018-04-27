8ac478baf8411f5cd97824e1d3878ca21f526428
David Jabulani <h140476z@hit.ac.zw>
2018-04-27 13:43:01 +0200

    Add project files.

---
+++ antiokas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(antiokas.Startup))]
+namespace antiokas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
